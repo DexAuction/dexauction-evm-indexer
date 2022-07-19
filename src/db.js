c1e79a81224b0e0306ca58951c4a1431d6ef9780
purnimaagrawal <purnima.agrawal1992@gmail.com>
2022-07-19 16:37:54 +0530

    Update environment variables

--- src/db.js
+++ src/db.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
-const MONGO_URI = `mongodb+srv://admin:admin@testcluster.f41zx.mongodb.net/DEXA?authSource=admin&replicaSet=atlas-3mrvyl-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true`;
-// const MONGO_URI = `mongodb+srv://admin:admin@testcluster.f41zx.mongodb.net/Dexa-Rinkeby?authSource=admin&replicaSet=atlas-3mrvyl-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true`;
-console.log(`Connecting MongoDB at ${MONGO_URI}`);
+const MONGO_URI = process.env.MONGO_URI;
 async function connect() {
   await mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
