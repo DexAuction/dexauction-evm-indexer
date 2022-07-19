17f1f80dc1daf19c71a847542bb90cf362620c38
purnimaagrawal <purnima.agrawal1992@gmail.com>
2022-07-19 16:30:33 +0530

    Add support for Decentraland and ENS NFT Token

--- src/abi/index.js
+++ src/abi/index.js
@@ -1,11 +1,13 @@
-const ENGLISH_AUCTION_ABI = require('./EnglishAuction');
-const PROXY_AUCTION_ABI = require('./DexAuction');
-const NFT_CONTRACT_ABI = require('./Token');
-const DUTCH_CONTRACT_ABI = require('./DutchAuction');
+const ENGLISH_AUCTION_ABI = require("./EnglishAuction");
+const PROXY_AUCTION_ABI = require("./DexAuction");
+const DECENTRALAND_NFT_CONTRACT_ABI = require("./DecentralandToken");
+const ENS_NFT_CONTRACT_ABI = require("./ENSToken");
+const DUTCH_CONTRACT_ABI = require("./DutchAuction");
 
 module.exports = {
   ENGLISH_AUCTION_ABI,
   PROXY_AUCTION_ABI,
-  NFT_CONTRACT_ABI,
-  DUTCH_CONTRACT_ABI
+  DECENTRALAND_NFT_CONTRACT_ABI,
+  ENS_NFT_CONTRACT_ABI,
+  DUTCH_CONTRACT_ABI,
 };
