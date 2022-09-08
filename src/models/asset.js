deecebd361e15f853708b0b197615f08805ff025
purnimaagrawal <purnima.agrawal1992@gmail.com>
2022-09-08 10:59:58 +0530

    Make status enum

--- src/models/asset.js
+++ src/models/asset.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const {DEFAULT_ASSET_STATUS,ON_SALE_ASSET_STATUS} = require("../constants")
 
 const asset = new mongoose.Schema({
   asset_id: {
@@ -6,6 +7,7 @@ const asset = new mongoose.Schema({
   },
   status: {
     type: String,
+    enum: [DEFAULT_ASSET_STATUS,ON_SALE_ASSET_STATUS],
   },
   assetContractAddress: {
     type: String,
